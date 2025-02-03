2ab7ce6c3e3b8f3362e0279e4e7d96e86318bd3b
ravinder-khera <ravinder.decypt@gmail.com>
2025-02-03 09:29:33 +0530

    getTimelineJobs api added

--- src/services/auth.js
+++ src/services/auth.js
@@ -413,6 +413,32 @@ export const getJobs = async (page) => {
     }
 };
 
+export const getTimelineJobs = async (start_date, end_date) => {
+    const authToken = localStorage.getItem('authToken');
+    const requestOptions = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+          "Authorization": `Bearer ${authToken}`,
+        }
+    };
+    try {
+        let response = await fetch(`${process.env.REACT_APP_USER_API_CLOUD_ENDPOINT}/v2/jobs/timeline?start_date=${start_date}&end_date=${end_date}`, requestOptions);
+        const isJson = response.headers.get("content-type")?.includes("application/json");
+        const data = isJson && (await response.json());
+        console.log(response,data);
+        if(response.status === 200){
+            return { res: data, error: null } ;
+        }else{
+            return { res: null, error: data } ;
+        }
+    } catch (error) {
+        console.error("There was an error!", error);
+        return { res: null, error: error }
+    }
+};
+
 export const getSingleJob = async (id) => {
     const authToken = localStorage.getItem('authToken');
     const requestOptions = {
